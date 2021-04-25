c49ffe60e9aa7c8a6cdd8791f27de7ce5b073270
v-briese <valentine.briese.07@gmail.com>
2021-04-24 22:51:22 -0700

    Put way too much effort into making an error message

--- components/Preview.jsx
+++ components/Preview.jsx
@@ -7,33 +7,55 @@ import { Spinner } from "@vizality/components";
 export default class Preview extends Component {
 	render() {
 		const UserProfile = getModuleByDisplayName("UserProfile");
+		const HelpMessage = getModuleByDisplayName("HelpMessage");
 		const { getCurrentUser } = getModule("getCurrentUser");
 
 		return (
-			<>
-				<Spinner id="custom-rich-presence-preview-spinner" />
-				<div id="custom-rich-presence-preview" style={{ display: "none" }}>
-					<FormItem title="Preview">
+			<div id="custom-rich-presence-preview-container">
+				<FormItem title="Preview">
+					<Spinner id="custom-rich-presence-preview-spinner" />
+					<div
+						id="custom-rich-presence-preview-notice"
+						style={{ display: "none" }}
+					>
+						<HelpMessage messageType={2}>
+							It seems that I am unable to set your Rich Presence, this is
+							likely due to game activity being disabled. Please try enabling
+							it. When done, reload this page.
+						</HelpMessage>
+					</div>
+
+					<div id="custom-rich-presence-preview" style={{ display: "none" }}>
 						<UserProfile user={getCurrentUser()} />
-					</FormItem>
-				</div>
-			</>
+					</div>
+				</FormItem>
+			</div>
 		);
 	}
 
 	componentDidMount() {
 		setTimeout(() => {
 			const profile = document.getElementById("custom-rich-presence-preview")
-				.children[0].children[1];
-
+				.children[0];
 			profile.style = "width: auto;";
 			profile.children[1].remove();
 			profile.children[0].children[0].remove();
 
 			document.getElementById("custom-rich-presence-preview-spinner").style =
 				"display: none;";
-			document.getElementById("custom-rich-presence-preview").style =
+			document.getElementById("custom-rich-presence-preview-container").style =
 				"top: 40px; position: sticky;";
+
+			if (
+				!document.getElementsByClassName(
+					getModule("activityProfile").activityProfile
+				).length
+			)
+				return (document.getElementById(
+					"custom-rich-presence-preview-notice"
+				).style = "");
+
+			document.getElementById("custom-rich-presence-preview").style = "";
 		}, 500);
 	}
 }
